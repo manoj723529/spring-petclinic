8a60b440a84c8ac429a83d31939bf0eb0b144a8d
manoj723529 <jamimanojkumar135@gmail.com>
2026-04-01 17:23:26 +0530

    Create route.tf
    Signed-off-by: manoj723529 <jamimanojkumar135@gmail.com>

---
+++ terraform/route.tf
@@ -0,0 +1,21 @@
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "private_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
