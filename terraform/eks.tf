43ff17f6b8b92aa0593b6776114b3ea1b0a83373
manoj723529 <jamimanojkumar135@gmail.com>
2026-04-06 09:17:06 +0530

    Update eks.tf
    Signed-off-by: manoj723529 <jamimanojkumar135@gmail.com>

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,8 +2,8 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "petclinic-cluster"
-  cluster_version = "1.29"
+  cluster_name    = "springpetclinic-cluster"
+  cluster_version = "1.30"
 
   vpc_id     = aws_vpc.main.id
   subnet_ids = aws_subnet.private[*].id
