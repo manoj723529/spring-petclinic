94f3ba698db68e5c1cd621dc9c43fecebd038f36
manoj723529 <jamimanojkumar135@gmail.com>
2026-04-06 11:27:49 +0530

    Update eks.tf
    Signed-off-by: manoj723529 <jamimanojkumar135@gmail.com>

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -6,14 +6,28 @@ module "eks" {
   cluster_version = "1.30"
 
   vpc_id     = aws_vpc.main.id
-  subnet_ids = aws_subnet.private[*].id
+  subnet_ids = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id
+  ]
+
+  # 🔥 VERY IMPORTANT
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  # Allow your Jenkins IP
+  cluster_endpoint_public_access_cidrs = ["65.0.185.177/32"]
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      max_size     = 4
-      min_size     = 2
+      desired_size   = 2
+      max_size       = 4
+      min_size       = 2
       instance_types = ["c7i-flex.large"]
     }
+  }
+
+  tags = {
+    Environment = "dev"
   }
 }
