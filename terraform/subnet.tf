45dd257e2e3c2903c4342dd1c829a477a699ac61
manoj723529 <jamimanojkumar135@gmail.com>
2026-04-06 12:22:44 +0530

    Update subnet.tf
    Signed-off-by: manoj723529 <jamimanojkumar135@gmail.com>

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,17 +1,42 @@
-# Public Subnets
+# -------------------------
+# PUBLIC SUBNETS
+# -------------------------
 resource "aws_subnet" "public" {
   count = 2
-  vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 8, count.index)
-  availability_zone = element(["ap-south-1a","ap-south-1b"], count.index)
 
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
+  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${count.index}"
+
+    # 🔥 REQUIRED for EKS LoadBalancer
+    "kubernetes.io/role/elb" = "1"
+
+    # 🔥 REQUIRED for EKS cluster
+    "kubernetes.io/cluster/springpetclinic-cluster" = "shared"
+  }
 }
 
-# Private Subnets
+# -------------------------
+# PRIVATE SUBNETS
+# -------------------------
 resource "aws_subnet" "private" {
   count = 2
-  vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 8, count.index + 10)
-  availability_zone = element(["ap-south-1a","ap-south-1b"], count.index)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet("10.0.0.0/16", 8, count.index + 10)
+  availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
+
+  tags = {
+    Name = "private-subnet-${count.index}"
+
+    # 🔥 REQUIRED for internal load balancer
+    "kubernetes.io/role/internal-elb" = "1"
+
+    # 🔥 REQUIRED for EKS cluster
+    "kubernetes.io/cluster/springpetclinic-cluster" = "shared"
+  }
 }
